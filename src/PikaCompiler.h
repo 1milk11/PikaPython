8a607ad8f089c949b2a42cdce0f5d695f25c85f3
AZhao <2223757448@qq.com>
2024-07-11 01:04:45 +0800

    add compiler refactor plan

--- src/PikaCompiler.h
+++ src/PikaCompiler.h
@@ -12,26 +12,13 @@ PIKA_RES pikaCompileFileWithOutputName(char* output_file_name,
                                        char* input_file_name);
 PIKA_RES pikaCompile(char* output_file_name, char* py_lines);
 
-LibObj* New_LibObj(Args* args);
-void LibObj_deinit(LibObj* self);
-void LibObj_dynamicLink(LibObj* self, char* module_name, uint8_t* bytecode);
-int LibObj_staticLink(LibObj* self,
-                      char* module_name,
-                      uint8_t* bytecode,
-                      size_t size);
-int LibObj_staticLinkFile(LibObj* self, char* input_file_name);
-void LibObj_listModules(LibObj* self);
-int LibObj_linkFile(LibObj* self, char* output_file_name);
+LibObj* New_LibObj(Args* args);     // TODO: why it's in the header
 int LibObj_loadLibraryFile(LibObj* self, char* input_file_name);
 PikaObj* LibObj_getModule(LibObj* self, char* module_name);
-int Lib_loadLibraryFileToArray(char* origin_file_name, char* pikascript_root);
 char* LibObj_redirectModule(LibObj* self, Args* buffs, char* module_name);
 PikaMaker* New_PikaMaker(void);
 void pikaMaker_setPWD(PikaMaker* self, char* pwd);
-PIKA_RES pikaMaker_compileModule(PikaMaker* self, char* module_name);
-int pikaMaker_getDependencies(PikaMaker* self, char* module_name);
-void pikaMaker_printStates(PikaMaker* self);
-char* pikaMaker_getFirstNocompiled(PikaMaker* self);
+void pikaMaker_printStates(PikaMaker* self);    // TODO: debug interface
 PIKA_RES pikaMaker_compileModuleWithDepends(PikaMaker* self, char* module_name);
 PIKA_RES pikaMaker_linkCompiledModulesFullPath(PikaMaker* self, char* lib_path);
 PIKA_RES pikaMaker_linkCompiledModules(PikaMaker* self, char* lib_name);
@@ -45,10 +32,6 @@ PIKA_RES pikaMaker_linkRaw(PikaMaker* self, char* file_path);
 PIKA_RES pikaMaker_linkRawWithPath(PikaMaker* self,
                                    char* file_path,
                                    char* pack_path);
-PIKA_RES _loadModuleDataWithName(uint8_t* library_bytes,
-                                 char* module_name,
-                                 uint8_t** addr_p,
-                                 size_t* size_p);
 
 #define LIB_VERSION_NUMBER 7
 #define LIB_INFO_BLOCK_SIZE 32
